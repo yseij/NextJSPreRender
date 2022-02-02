ac442b378cb45bc90813f8cefdf73165cfa3c71a
yseij <r0790156@student.thomasmore.be>
2022-02-02 21:01:57 +0100

    Fallback pagina

--- pages/[pid].js
+++ pages/[pid].js
@@ -4,6 +4,10 @@ import fs from "fs/promises";
 
 function ProductDetailPage(props) {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    return <p>Loading...</p>;
+  }
   return (
     <Fragment>
       <h1>{loadedProduct.title}</h1>
@@ -36,14 +40,8 @@ export async function getStaticPaths() {
       {
         params: { pid: "p1" },
       },
-      {
-        params: { pid: "p2" },
-      },
-      {
-        params: { pid: "p3" },
-      },
     ],
-    fallback: false,
+    fallback: true,
   };
 }
 
